85da01a32655425b8a28668293978e46ac51a97a
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2024-03-14 07:07:16 -0600

    update

--- titlle.js
+++ titlle.js
@@ -2,4 +2,4 @@ MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 var observer = new MutationObserver(function(mutations, observer) {
   document.title = document.title.replace(/^\(\d+\)\s*/, '');
 });
-observer.observe(document.querySelector('title'), {childList: true});
+observer.observe(document, {childList: true});
